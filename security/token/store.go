e7581b2e477617f8c037e779aeffdc6041838452
Mattias <traed@users.noreply.github.com>
2025-09-10 16:40:45 +0200

    Only use primitive types in token store (#5)

--- security/token/store.go
+++ security/token/store.go
@@ -10,14 +10,14 @@ type Store[T any] interface {
 	// Looks up a token in the underlying token store, and returns its corresponding user roles.
 	// A user can have 0+ roles. If the token doesn't exist in store and/or has been revoked, it MUST
 	// return an error. The ctx MIGHT be a *papi.RequestCtx.
-	UserRoles(ctx context.Context, tok Token) (roles []string, err error)
+	UserRoles(ctx context.Context, tok string) (roles []string, err error)
 
 	// Consume an authentication code and returns its corresponding details.
-	ConsumeAuthCode(ctx context.Context, code OneTimeCode) (userId T, cookie bool, err error)
+	ConsumeAuthCode(ctx context.Context, code string) (userId T, cookie bool, err error)
 
 	// Save an authentication code to a storage.
-	SaveAuthCode(ctx context.Context, userId T, code OneTimeCode, expiry time.Time, cookie bool) error
+	SaveAuthCode(ctx context.Context, userId T, code string, expiry time.Time, cookie bool) error
 
 	// Save an access token to a storage. Only the ID of the token should be saved, not the whole token.
-	SaveAccessToken(ctx context.Context, userId T, tok Token, cookie bool) error
+	SaveAccessToken(ctx context.Context, userId T, tok string, cookie bool) error
 }
